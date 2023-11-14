417abd9b1e18bdf18146a0fa54e581add4795675
kayasem <kayasemvc@gmail.com>
2023-11-14 15:50:55 +0100

    programmed binomial calculation

--- src/main/kotlin/binomialDistribution.kt
+++ src/main/kotlin/binomialDistribution.kt
@@ -1,6 +1,22 @@
 
-    fun calculateBinomialDistribution(amount: Int, succes_rate: Float){
+import kotlin.math.pow
+
+// n = number of trials
+// p = succesrate
+// x = a value from 0 to n. It represents a possible outcome. It represents a random_variable X
+
+// Calculating a binomial distribution means calculation the chance for every X (random_variable) that is possible in the trials, from 0 to n
+fun calculateBinomialDistribution(n: Int, p: Double, k: Int): Double{
 
 
-        println("binom calc")
+        // calculate (1-p)^n-k
+        val chance = (1-p).pow(n-k)
+
+
+
+        return calculateBinomial(n, k) * p.pow(k)*chance
+
+
+
     }
+
