21b0cbc2b9ddda5b388c5ec524560a61694a85f3
kayasem <kayasemvc@gmail.com>
2023-11-15 09:15:12 +0100

    Functioning binomial distribution that prints out the chance of each random variable from 0 to n

--- src/main/kotlin/binomialDistribution.kt
+++ src/main/kotlin/binomialDistribution.kt
@@ -6,17 +6,19 @@ import kotlin.math.pow
 // x = a value from 0 to n. It represents a possible outcome. It represents a random_variable X
 
 // Calculating a binomial distribution means calculation the chance for every X (random_variable) that is possible in the trials, from 0 to n
-fun calculateBinomialDistribution(n: Int, p: Double, k: Int): Double{
+fun calculateBinomialDistribution(n: Int, p: Double){
 
+    var k = 0
 
         // calculate (1-p)^n-k
         val chance = (1-p).pow(n-k)
 
-
-
-        return calculateBinomial(n, k) * p.pow(k)*chance
-
+    while (k < n+1){
+        println("The change of the experiment returning $k times true is: ${ calculateBinomial(n, k) * p.pow(k) * chance }")
+        k++
+    }
 
 
     }
+
 
